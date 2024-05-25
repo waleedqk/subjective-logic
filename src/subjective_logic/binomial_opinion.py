553bf126f46d984d44d5c7b716bd0c58ff60ed93
Waleed Khan <waleedqk@gmail.com>
2024-05-24 22:52:05 -0400

    BinomialOpinion with 3 operators

--- src/subjective_logic/binomial_opinion.py
+++ src/subjective_logic/binomial_opinion.py
@@ -1,4 +1,5 @@
 import math
+from decimal import Decimal, ROUND_HALF_UP
 from typing import Annotated, Union
 
 class BinomialOpinion:
@@ -21,6 +22,12 @@ class BinomialOpinion:
             uncertainty (float): The uncertainty component of the opinion.
             base_rate (float): The base rate (default 0.5).
         """
+
+        self._belief = None
+        self._disbelief = None
+        self._uncertainty = None
+        self._base_rate = None
+
         self.set_param('belief', belief)
         self.set_param('disbelief', disbelief)
         self.set_param('uncertainty', uncertainty)
@@ -31,34 +38,67 @@ class BinomialOpinion:
         """
         Set the value of a parameter of the opinion object.
         """
-        setattr(self, attribute, self._validate_value(self._set_to_zero_if_close(value)))
+        # 1.000000 means we want to keep up to 6 decimal places.
+        value = Decimal(value).quantize(Decimal("1.000000"), rounding=ROUND_HALF_UP)
+        setattr(self, f'_{attribute}', self._validate_value(self._set_to_zero_if_close(value, n_decimal_places=6)))
 
-    def _set_to_zero_if_close(self, value, n_decimal_places = 6):
+
+    def _set_to_zero_if_close(self, value, n_decimal_places=6):
         # Threshold for closeness to zero
-        threshold = 10 ** (-n_decimal_places)  
-
+        threshold = Decimal(10) ** (-n_decimal_places)
         if abs(value) < threshold:
-            return 0
+            return Decimal(0)
         else:
             return value
-
+        
     def _validate_value(self, value):
-
-        # Validate that the input is a float between 0 and 1
-        if not isinstance(value, (float, int)):
-            raise ValueError("Value must be a float or an integer.")
-        if not 0 <= value <= 1:
+        # Validate that the input is a Decimal between 0 and 1
+        if not isinstance(value, Decimal):
+            raise ValueError("Value must be a Decimal.")
+        if not Decimal(0) <= value <= Decimal(1):
             raise ValueError("Value must be between 0 and 1.")
-        return float(value)        
+        return value
 
     def validate_opinion(self):
         """
         Validates the opinion to ensure the sum of belief, disbelief, and uncertainty equals 1.
         Raises an exception if the validation fails.
         """
-        total = self.belief + self.disbelief + self.uncertainty
-        if not (0.999 <= total <= 1.001):  # Allows a small margin for floating-point errors
+        total = self._belief + self._disbelief + self._uncertainty
+        if not (Decimal("0.999999") <= total <= Decimal("1.000001")):  # Allows a small margin for floating-point errors
             raise ValueError("The sum of belief, disbelief, and uncertainty must equal 1.")
+
+    @property
+    def belief(self):
+        return float(self._belief)
+
+    @belief.setter
+    def belief(self, value):
+        self.set_param('belief', value)
+
+    @property
+    def disbelief(self):
+        return float(self._disbelief)
+
+    @disbelief.setter
+    def disbelief(self, value):
+        self.set_param('disbelief', value)
+
+    @property
+    def uncertainty(self):
+        return float(self._uncertainty)
+
+    @uncertainty.setter
+    def uncertainty(self, value):
+        self.set_param('uncertainty', value)
+
+    @property
+    def base_rate(self):
+        return float(self._base_rate)
+
+    @base_rate.setter
+    def base_rate(self, value):
+        self.set_param('base_rate', value)
 
     def to_json(self):
         """
@@ -109,13 +149,15 @@ class BinomialOpinion:
         Raises:
             ValueError: If the calculated probability is not between 0 and 1.
         """
-        probability =  self.belief + self.base_rate * self.uncertainty
+        # Use Decimal for precise calculation
+        probability = self._belief + self._base_rate * self._uncertainty
 
-        ## add error check that the probability is between 0 and 1
-        if not 0 <= probability <= 1:
+        # Add error check that the probability is between 0 and 1
+        if not Decimal(0) <= probability <= Decimal(1):
             raise ValueError("Probability must be between 0 and 1.")
-        
-        return probability
+
+        # Convert to float before returning
+        return float(probability)
     
     @classmethod
     def from_evidence(cls, r, s, base_rate=0.5, W=2):
@@ -141,12 +183,12 @@ class BinomialOpinion:
         if not (isinstance(s, int) and s > 0):
             raise ValueError("Negative evidence 's' must be a positive integer.")
             
-        belief = r / (r + s + W)
-        disbelief = s / (r + s + W)
-        uncertainty = W / (r + s + W)
+        belief = Decimal(r) / (Decimal(r) + Decimal(s) + Decimal(W))
+        disbelief = Decimal(s) / (Decimal(r) + Decimal(s) + Decimal(W))
+        uncertainty = Decimal(W) / (Decimal(r) + Decimal(s) + Decimal(W))
 
         # Create a new instance of BinomialOpinion using the calculated values
-        return cls(belief, disbelief, uncertainty, base_rate)
+        return cls(float(belief), float(disbelief), float(uncertainty), base_rate)
 
     
     def __str__(self):
@@ -158,3 +200,79 @@ class BinomialOpinion:
         """
         return (f"BinomialOpinion(belief={self.belief:.3f}, disbelief={self.disbelief:.3f}, "
                 f"uncertainty={self.uncertainty:.3f}, base_rate={self.base_rate:.3f})")
+    
+    def __repr__(self):
+        return f"BinomialOpinion(belief={float(self.belief)}, disbelief={float(self.disbelief)}, uncertainty={float(self.uncertainty)}, base_rate={float(self.base_rate)})"    
+
+    def cumulative_fusion(self, other: "BinomialOpinion"):
+        """
+        Performs cumulative fusion with another BinomialOpinion object.
+        
+        :param other: Another BinomialOpinion object to fuse with
+        :return: A new BinomialOpinion object representing the fused opinion
+
+        Assume a long-term opinion about agent X, is calculated based on previous observations within a time window. 
+        And newly observed evidences forms a short-term opinion just on that window.
+        The updated BinomialOpinion is now over the entire time period vis the cumulative fusion opinion.
+        """
+        # Eq: 12.14
+
+        # Check for the edge case where both uncertainties are zero, which would cause division by zero
+        if self._uncertainty == 0 and other._uncertainty == 0:
+            raise ValueError("Cumulative fusion is undefined when both uncertainties are zero.")
+            # TODO: Add Eq: 12.15 from book for this case
+
+        # Calculate the fused belief, disbelief, uncertainty, and base rate
+        denominator = self._uncertainty + other._uncertainty - (self._uncertainty * other._uncertainty)
+        belief = ((self._belief * other._uncertainty) + (other._belief * self._uncertainty)) / denominator
+        disbelief = ((self._disbelief * other._uncertainty) + (other._disbelief * self._uncertainty)) / denominator
+        uncertainty = (self._uncertainty * other._uncertainty) / denominator
+
+
+        # Check for the special case where both As are 1
+        if self._uncertainty == 1 and other._uncertainty == 1:
+            base_rate = (self._base_rate + other._base_rate) / 2
+        else:
+            base_rate = ((self._base_rate * other._uncertainty) + (other._base_rate * self._uncertainty) - ((self._base_rate + other._base_rate) * self._uncertainty * other._uncertainty)) / \
+                        (self._uncertainty + other._uncertainty - (2 * self._uncertainty * other._uncertainty))
+        
+        return BinomialOpinion(float(belief), float(disbelief), float(uncertainty), float(base_rate))
+    
+
+    def trust_discounting(self, other: "BinomialOpinion"):
+        """
+        Performs trust discounting with another opinion object.
+
+        The discounting operator combines A's trust in B, and B's opinion about X. Then to derive A's opinion about X, the discounting operator is used:
+        """
+
+        # Eq: 14.6
+        # Calculate the discounted belief, disbelief, and uncertainty using Decimal
+        belief = Decimal(self.probability()) * other._belief
+        disbelief = Decimal(self.probability()) * other._disbelief
+        uncertainty = Decimal(1) - belief - disbelief
+        base_rate = other._base_rate
+
+        # Convert the results to float before creating the new BinomialOpinion
+        return BinomialOpinion(float(belief), float(disbelief), float(uncertainty), float(base_rate))
+
+    def average_fusion(self, other: "BinomialOpinion"):  
+        """
+        Performs average fusion with another opinion object.
+
+        Based on two opinions W_A_X (Opinion of A about X) and W_B_X (Opinion of B about X). The average fusion, i.e., the averaged opinion, of these two opinions is calculated as follows:
+        """
+       
+        if self._uncertainty == 0 and other._uncertainty == 0:
+            raise ValueError("Cumulative fusion is undefined when both uncertainties are zero.")
+            # TODO: Add Eq: 12.19 from book for this case
+       
+        if self._uncertainty != 0 or other._uncertainty != 0:
+            belief = ((self._belief * other._uncertainty) + (other._belief * self._uncertainty)) / (self._uncertainty + other._uncertainty)
+            disbelief = ((self._disbelief * other._uncertainty) + (other._disbelief * self._uncertainty)) / (self._uncertainty + other._uncertainty)
+            uncertainty = (2 * self._uncertainty * other._uncertainty) / (self._uncertainty + other._uncertainty)
+            base_rate = (self._base_rate + other._base_rate) / 2
+
+        return BinomialOpinion(float(belief), float(disbelief), float(uncertainty), float(base_rate))
+
+# TODO: Weighted Belief Fusion (Eq: 12.22)
--- tests/test_binomial_opinion.py
+++ tests/test_binomial_opinion.py
@@ -3,31 +3,76 @@ import pytest
 
 def test_binomial_opinion_initialization():
     opinion = BinomialOpinion(belief=0.5, disbelief=0.3, uncertainty=0.2, base_rate=0.5)
-    assert opinion.belief == 0.5
-    assert opinion.disbelief == 0.3
-    assert opinion.uncertainty == 0.2
-    assert opinion.base_rate == 0.5
+    assert opinion.belief == pytest.approx(0.5)
+    assert opinion.disbelief == pytest.approx(0.3)
+    assert opinion.uncertainty == pytest.approx(0.2)
+    assert opinion.base_rate == pytest.approx(0.5)
 
-# write a test to compare opinion when using json serialization
 def test_binomial_opinion_to_from_json():
     opinion1 = BinomialOpinion(belief=0.5, disbelief=0.3, uncertainty=0.2, base_rate=0.5)
     opinion2 = BinomialOpinion.from_json(opinion1.to_json())
     
-    compare_opinions(opinion1, opinion2)
+    compare_binomial_opinions(opinion1, opinion2)
 
-# create a test for the from_evidence method
 def test_binomial_opinion_from_evidence():
     opinion = BinomialOpinion.from_evidence(r=2, s=1, base_rate=0.5, W=2)
-    assert opinion.belief == 0.4
-    assert opinion.disbelief == 0.2
-    assert opinion.uncertainty == 0.4
-    assert opinion.base_rate == 0.5
+    assert opinion.belief == pytest.approx(0.4)
+    assert opinion.disbelief == pytest.approx(0.2)
+    assert opinion.uncertainty == pytest.approx(0.4)
+    assert opinion.base_rate == pytest.approx(0.5)
 
-def compare_opinions(opinion1, opinion2):
+def test_binomial_opinion_cumulative_fusion():
+    opinion1 = BinomialOpinion(belief=0.5, disbelief=0.3, uncertainty=0.2, base_rate=0.5)
+    opinion2 = BinomialOpinion(belief=0.6, disbelief=0.2, uncertainty=0.2, base_rate=0.5)
+    fused_opinion = opinion1.cumulative_fusion(opinion2)
+
+    validate_binomial_opinion(fused_opinion)
+    
+    # specify a custom tolerance for the comparison - sets the relative tolerance to 1% of the expected value
+    assert fused_opinion.belief == pytest.approx(0.611, rel=1e-2)
+    assert fused_opinion.disbelief == pytest.approx(0.278, rel=1e-2)
+    assert fused_opinion.uncertainty == pytest.approx(0.111, rel=1e-2)
+    assert fused_opinion.base_rate == pytest.approx(0.5, rel=1e-2)
+
+def test_binomial_opinion_trust_discounting():
+    opinion1 = BinomialOpinion(belief=0.2, disbelief=0.4, uncertainty=0.4, base_rate=0.75)
+    opinion2 = BinomialOpinion(belief=0.45, disbelief=0.35, uncertainty=0.2, base_rate=0.25)
+    trust_discounted_opinion = opinion1.trust_discounting(opinion2)
+
+    validate_binomial_opinion(trust_discounted_opinion)
+    
+    # specify a custom tolerance for the comparison - sets the relative tolerance to 1% of the expected value
+    # Eq: 14.8
+    assert trust_discounted_opinion.belief == pytest.approx(0.225, rel=1e-2)
+    assert trust_discounted_opinion.disbelief == pytest.approx(0.175, rel=1e-2)
+    assert trust_discounted_opinion.uncertainty == pytest.approx(0.600, rel=1e-2)
+    assert trust_discounted_opinion.base_rate == pytest.approx(0.250, rel=1e-2)
+
+def test_binomial_opinion_average_fusion():
+    opinion1 = BinomialOpinion(belief=0.55, disbelief=0.21, uncertainty=0.24, base_rate=0.28)
+    opinion2 = BinomialOpinion(belief=0.16, disbelief=0.64, uncertainty=0.2, base_rate=0.82)
+    average_fused_opinion = opinion1.average_fusion(opinion2)
+
+    validate_binomial_opinion(average_fused_opinion)
+    
+    # specify a custom tolerance for the comparison - sets the relative tolerance to 1% of the expected value
+    assert average_fused_opinion.belief == pytest.approx(0.34, rel=1e-2)
+    assert average_fused_opinion.disbelief == pytest.approx(0.4445, rel=1e-2)
+    assert average_fused_opinion.uncertainty == pytest.approx(0.22, rel=1e-2)
+    assert average_fused_opinion.base_rate == pytest.approx(0.55, rel=1e-2)
+
+def validate_binomial_opinion(opinion: "BinomialOpinion"):
+    assert 0 <= opinion.belief <= 1
+    assert 0 <= opinion.disbelief <= 1
+    assert 0 <= opinion.uncertainty <= 1
+    assert 0 <= opinion.base_rate <= 1
+    assert 0.999 <= opinion.belief + opinion.disbelief + opinion.uncertainty <= 1.001
+
+def compare_binomial_opinions(opinion1, opinion2):
     """
     Compare two BinomialOpinion instances.
     """
-    assert opinion1.belief == opinion2.belief
-    assert opinion1.disbelief == opinion2.disbelief
-    assert opinion1.uncertainty == opinion2.uncertainty
-    assert opinion1.base_rate == opinion2.base_rate
+    assert opinion1.belief == pytest.approx(opinion2.belief, rel=1e-2)
+    assert opinion1.disbelief == pytest.approx(opinion2.disbelief, rel=1e-2)
+    assert opinion1.uncertainty == pytest.approx(opinion2.uncertainty, rel=1e-2)
+    assert opinion1.base_rate == pytest.approx(opinion2.base_rate, rel=1e-2)
